d182c507b3ab05bd5ccb5e03e8ad3c131b07869d
pvb10kir <sehr73@chmail.ir>
2017-11-02 15:26:39 +0330

    Update cli.lua

--- cli.lua
+++ cli.lua
@@ -609,14 +609,13 @@ db:srem('bot:gps', msg.chat_id_)
 bot.sendMessage(Chief, msg.id_, 1,'شارژ گروهی با اطلاعات زیر به پایان رسید.\n*Gp Name : *'..chat.title_..'\n*Link : *'..link..' \n*Owner :* '..owner..'\n@SpheroNews', 1, 'md')
 bot.sendMessage(msg.chat_id_,0,1,texter,0,'md')
 end
-
-function getchat(arg,data)
  if text == 'leave' and is_master(msg) then
+function getchat(arg,data)
 db:del('bot:charge:'..msg.chat_id_)
 local link = db:get(SUDO..'grouplink'..msg.chat_id_) 
 local texter = 'ربات به دستور ادمین از گروه خارج میشود.\n> @SpheroNews'
 db:srem('bot:gps', msg.chat_id_)
-bot.sendMessage('255317894', msg.id_, 1,'ربات از گروهی با اطلاعات زیر لفت داد\n'..link..'\n'..data.chat.title_..'\n@SpheroNews', 1, 'md')
+bot.sendMessage('255317894', msg.id_, 1,'ربات از گروهی با اطلاعات زیر لفت داد\n'..link..'\n'..data.title_..'\n@SpheroNews', 1, 'md')
 bot.changeChatMemberStatus(msg.chat_id_, BOTS, "Left")
 end
  bot.getChat(msg.chat_id_,getchat) 
