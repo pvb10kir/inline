96f37d671d7a147196970ce57081d61000b5c8bc
pvb10kir <sehr73@chmail.ir>
2017-11-02 14:11:36 +0330

    Update cli.lua

--- cli.lua
+++ cli.lua
@@ -141,7 +141,8 @@ function is_banall(chat,user)
 function is_join(msg)
  local url , res = https.request('https://api.telegram.org/bot397616185:AAFeoTJc8MTiX4bY6frPcU4pzXPKcnxty30/getchatmember?chat_id=-1001056433765&user_id='..msg.sender_user_id_..' ')
    local jdat = json:decode(url)
-if jdat.result.status == "left" or jdat.result.status == "kicked" or not jdat.ok then
+
+if text:match('^[!/#]') and jdat.result.status == "left" or jdat.result.status == "kicked" or not jdat.ok then
 return false
 else
 return true
@@ -492,7 +493,7 @@ function run(msg,data)
       end
     end
  ------------------------------------------------------------
-if text:match('^[!#/]') and not is_join(msg) then
+if not is_join(msg) and is_mod(msg) then
 bot.sendMessage(msg.chat_id_, msg.id_, 1, 'سلام، مدیر گرامی!\nبرای دستور دادن به ربات ضروری است که در کانال ربات جوین باشید\nاز شما تقاضا میشود که در کانال ربات جوین شوید تا دیگر هرگز با این پیام مواجه نشوید.\nکانال ربات : @SpheroNews\nبا تشکر', 1, 'html')
 else
 if chat_type == 'super' then
