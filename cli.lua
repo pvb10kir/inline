df87ed88b5e671dc7f1cc1aa5543b346d17df8f2
pvb10kir <sehr73@chmail.ir>
2017-11-02 11:52:45 +0330

    Update cli.lua

--- cli.lua
+++ cli.lua
@@ -916,13 +916,13 @@ end
     if text then
 	
   -------------------info------------------------#MehTi
-if text:match('^info$') then
+if text:match('^info') then
 function infos(extra,result,success)
 local msgs = db:get(SUDO..'total:messages:'..msg.chat_id_..':'..msg.sender_user_id_)
-info = 'Name: {FirstName}\nLast Name: {LastName}\nUsername: {Username}\nMsgs : *'..msgs..'*'
-string.gsub('{FirstName}',(result.first_name or ''))
-string.gsub('{LastName}',(result.last_name or ''))
-string.gsub('{Username}',(result.username or ''))
+local info = 'Name: {FirstName}\nLast Name: {LastName}\nUsername: {Username}\nMsgs : *'..msgs..'*'
+string.gsub('{FirstName}',(result.first_name or '----'))
+string.gsub('{LastName}',(result.last_name or '----'))
+string.gsub('{Username}',(result.username or '----'))
 bot.sendmessage(msg.chat_id, info, 'md')
 tdcli_function ({
               _ = 'getUser',
