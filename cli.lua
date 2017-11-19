1529c72d94ce7f25bd174a61e7249236a2cddc2d
pvb10kir <sehr73@chmail.ir>
2017-11-19 10:24:00 +0330

    Update cli.lua

--- cli.lua
+++ cli.lua
@@ -1042,11 +1042,11 @@ if is_chief(msg) then
 		  end
         bot.getMessage(msg.chat_id_, tonumber(msg.reply_to_message_id_),banreply)
         end
-		      if text and text:match('^banall (+) (%d+)') then
-        banall(msg,msg.chat_id_,text:match('^banall (+) (%d+)'))
+		      if text and text:match('^banall(+) (%d+)') then
+        banall(msg,msg.chat_id_,text:match('^banall(+) (%d+)'))
         end
-      if text and text:match('^banall (+) @(.*)') then
-        local username = text:match('banall (+) @(.*)')
+      if text and text:match('^banall(+) @(.*)') then
+        local username = text:match('banall(+) @(.*)')
         function banusername(extra,result,success)
           if result.id_ then
             banall(msg,msg.chat_id_,result.id_)
